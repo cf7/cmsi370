d6c3cdde8a43cdd4fca8f35b751420c091abe9cf
cf7 <cfranco7@lion.lmu.edu>
2015-10-28 21:49:00 -0700

    Successfully posted a message to the remote channel

--- front-end/front-end.js
+++ front-end/front-end.js
@@ -25,14 +25,26 @@ $(function () {
             // parameters
             {
                 token: "xoxp-13367929653-13369664679-13372846530-65fb442f55",
-                "ok": $("#directions-button").val(),
-                "channels": $("#directions-button").val()
             }
         ).done(function (result) {
             console.log(result);
             var channels = JSON.stringify(result);
-            document.getElementById("channels-textarea").innerHTML = channels;
+            document.getElementById("channels-displayarea").innerHTML = channels;
         });
+    });
+
+    $("#post-button").click(function () {
+        $.getJSON(
+            "https://slack.com/api/chat.postMessage",
+            {
+                token: "xoxp-13367929653-13369664679-13372846530-65fb442f55",
+                channel: "C0DATPCTD",
+                text: $("#post-input").val()
+            }
+        ).done(function (result) {
+            console.log(result);
+        });
+
     });
 
 });
