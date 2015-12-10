1f704113d07d254f463ade1c322be2138d75489b
cf7 <cfranco7@lion.lmu.edu>
2015-12-09 20:03:31 -0800

    Working on dropping tiles

--- widget-from-scratch/restructure.js
+++ widget-from-scratch/restructure.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-    var dragTile;
+    var $dragTile;
 
     var dragHandler = function (event) {
         // shift tiles to give preview of drop
@@ -8,13 +8,18 @@
         /**
         * Have surrounding tiles rearrange themselves to
         * preview tile drop
+        * (maybe not)
         */
 
-        dragTile.offset({ 
-            left: event.pageX - dragTile.deltaX, 
-            top: event.pageY - dragTile.deltaY
+        $dragTile.offset({ 
+            left: event.pageX - $dragTile.deltaX, 
+            top: event.pageY - $dragTile.deltaY
         });
+
+       $("body").data("mouseCoordinates", { left: event.pageX, top: event.pageY });
     };
+
+
     var dragCleanUp = function (event) {
         /**
         * if user decides not to drop, revert back to original positions
@@ -25,34 +30,69 @@
         * Use clone to place new div in drop location and delete original
         * tile
         */
-
-        dragTile.remove();
+        var mousePosition = $("body").data("mouseCoordinates");
+        $dragTile.hide();
+        var $element = $(document.elementFromPoint(mousePosition.left, mousePosition.top));
+        $dragTile.show();
+        console.log($element);
+        if ($element.hasClass("tile") || $element.parents().hasClass("tile")) {
+            $dragTile.hide();
+            $dragTile.insertAfter($element);
+            $dragTile.remove();
+        } else {
+            $dragTile.parent().append($dragTile);
+        }
         $("body").unbind("mousemove", dragHandler);
     };
+
+    var move = function () {
+        /**
+        * 2 instances for moving: 1.) An empty space has opened, move to empty space
+        * if it is this tile's turn
+        * 2.) 
+        */
+    };
+
+
+    // might not need this
+    var hoverHandler = function (event) {
+        console.log("hovering");
+        /**
+        * Dynamics of communicating with surrounding tiles and knowing when and
+        * where to move.
+        */
+    };
+
     var addDivs = function () {
-        $('div.col-sm-4').each(function (index) {
+        // look into using selectors $("div[class|='col']").each( . . .)
+        $(".row").children().each(function (index) {
             var $col = $(this);
             $col.wrap("<div class='tile'></div>");
         });
+        $(".tile").each(function (index) {
+            var $tile = $(this);
+            $tile.mouseover(hoverHandler); // mouseover() or hover(), why different functions?
+        });
     }
-    $.fn.restructure = function () {
 
+    $.fn.restructure = function () {
         addDivs();
 
         this.mousedown(function (event) {
-            if ($(event.target).parent().hasClass("tile")) {
-                dragTile = $(event.target).clone();
-                var startOffset = $(event.target).offset();
-                dragTile.deltaX = event.pageX - startOffset.left;
-                dragTile.deltaY = event.pageY - startOffset.top;
-                dragTile.addClass("tile-being-dragged")
+            $dragTile = $(event.target);
+            if ($dragTile.parent().hasClass("tile")) {
+                var startOffset = $dragTile.offset();
+                $dragTile.data("originalPosition", startOffset);
+                $dragTile.deltaX = event.pageX - startOffset.left;
+                $dragTile.deltaY = event.pageY - startOffset.top;
+                $dragTile.addClass("tile-being-dragged")
                         .offset({
-                            left: event.pageX - dragTile.deltaX,
-                            top: event.pageY - dragTile.deltaY
+                            left: event.pageX - $dragTile.deltaX,
+                            top: event.pageY - $dragTile.deltaY
                         });
-                $("body").append(dragTile);
+                $("body").append($dragTile);
                 $("body").mousemove(dragHandler);
-                dragTile.mouseup(dragCleanUp);
+                $dragTile.mouseup(dragCleanUp);
             }
         });
     };
