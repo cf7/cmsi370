d6ee4f5beafc520f9b42e36a082409f31ff9747c
cf7 <cfranco7@lion.lmu.edu>
2015-12-11 01:56:08 -0800

    Calibrating plugin for general html structure

--- front-end/front-end.js
+++ front-end/front-end.js
@@ -1,5 +1,6 @@
 $(function () {
-
+    $(document).restructure();
+    
     $.getJSON(
         // JD: 6
         "https://slack.com/api/channels.list",
--- widget-from-scratch/restructure.js
+++ widget-from-scratch/restructure.js
@@ -1,18 +1,38 @@
 (function ($) {
 
+    /**
+    * The goal of this plugin was to provide functionality that would allow users
+    * to rearrange the layout of their interfaces. It accomplishes this by
+    * wrapping existing html elements in the plugin's tiles that allow the plugin
+    * to then identify and move html elements from tile to tile.
+    */
+
+    /**
+    * Plugin may not swap tiles or highlight properly while the developer console is open
+    */
+
     var dragTile;
 
     var dragHandler = function (event) {
+        $(".underTile").css({ border: "" });
+        $(".underTile").removeClass("underTile");
         dragTile.offset({ 
             left: event.pageX - dragTile.deltaX, 
             top: event.pageY - dragTile.deltaY
         });
-
-       $("body").data("mouseCoordinates", { left: event.pageX, top: event.pageY });
+        $("body").data("mouseCoordinates", { left: event.pageX, top: event.pageY });
+        var mousePosition = $("body").data("mouseCoordinates");
+        dragTile.hide();
+        var $element = $(document.elementFromPoint(mousePosition.left, mousePosition.top));
+        dragTile.show();
+        $element.addClass("underTile");
+        $(".underTile").css({ border: "5px dotted blue" });
     };
 
 
     var dragCleanUp = function (event) {
+        $(".underTile").css({ border: "" });
+        $(".underTile").removeClass("underTile");
         var mousePosition = $("body").data("mouseCoordinates");
         dragTile.hide();
         var $element = $(document.elementFromPoint(mousePosition.left, mousePosition.top)); //coudn't find jQuery equivalent
@@ -23,9 +43,12 @@
             $("#sendingTile").append($element);
             $("#receivingTile").append($("#original"));
             dragTile.remove();
-            console.log("afterremove");
             $(".tile").removeAttr("id");
             $("#original").removeAttr("id");
+        } else {
+            $("#original").removeAttr("id");
+            $(".tile").removeAttr("id");
+            dragTile.remove();
         }
         $("body").unbind("mousemove", dragHandler);
     };
@@ -48,7 +71,7 @@
     };
 
     var addDivs = function () {
-        $("div[class|='col']").each(function (index) {
+        $(".col-sm-4, .col-sm-6").each(function (index) {
             var $col = $(this);
             $col.wrap("<div class='tile'></div>");
         });
@@ -66,8 +89,8 @@
         */
 
         this.mousedown(function (event) {
-            $(event.target).attr("id", "original");
             if ($(event.target).parent().hasClass("tile")) {
+                $(event.target).attr("id", "original");
                 dragTile = $(event.target).clone();
                 dragTile.attr("id", "tile-being-dragged");
                 var startOffset = $(event.target).offset(); // do not use the offset of the clone!
